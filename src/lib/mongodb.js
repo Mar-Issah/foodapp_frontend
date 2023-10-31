513137f1931d3c65bf09827881a09e3a553398d7
Mar-Issah <masy370@gmail.com>
2023-10-31 00:09:26 -0500

    fix error in mongodb config

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-const DB_URI = 'mongodb+srv://Marsiya:Database@cluster0.mjusc.mongodb.net/ham_food?retryWrites=true&w=majority';
 export const connectMongodb = async () => {
   try {
     await mongoose.connect(process.env.DB_URI);
