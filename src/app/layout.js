169d4b435ba10706c1a5572d926d1ffa82b5bce8
Mar-Issah <masy370@gmail.com>
2023-11-02 17:18:58 -0500

    merge and fix auth branch confilcts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 'use client';
 import { Inter } from 'next/font/google';
-import NextAuthProvider from './Provider';
+import NextAuthProvider from '@/lib/Provider';
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
@@ -14,22 +14,25 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <QueryClientProvider client={queryClient}>
-        <html lang='en'>
-          <head>
-            <title>H.A.M</title>
-            <meta name='H.A.M foods' content='Call now to make your food order' />
-            <link
-              rel='icon'
-              href='https://res.cloudinary.com/dytnpjxrd/image/upload/v1698682275/HAMFOODS/headicon_gd57zk.png'
-            />
-          </head>
-          <body className={inter.className}>{children}</body>
-        </html>
-        {/* </PersistGate> */}
-      </QueryClientProvider>
-    </Provider>
+    <html lang='en'>
+      <head>
+        <title>H.A.M</title>
+        <meta name='H.A.M foods' content='Call now to make your food order' />
+        <link
+          rel='icon'
+          href='https://res.cloudinary.com/dytnpjxrd/image/upload/v1698682275/HAMFOODS/headicon_gd57zk.png'
+        />
+      </head>
+      <Provider store={store}>
+        <NextAuthProvider>
+          {/* <PersistGate loading={null} persistor={persistor}> */}
+          <QueryClientProvider client={queryClient}>
+            <body className={inter.className}>{children}</body>
+
+            {/* </PersistGate> */}
+          </QueryClientProvider>
+        </NextAuthProvider>
+      </Provider>
+    </html>
   );
 }
