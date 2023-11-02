4f95e4518b01471582b431257ab209ee2ad8876a
Mar-Issah <masy370@gmail.com>
2023-11-01 22:53:36 -0500

    create order form modal

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import OrderForm from '@/components/OrderForm';
 import Button from '@/components/Button';
 import ButtonWrapper from '@/components/ButtonWrapper';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -15,23 +16,24 @@ import { useEffect, useState } from 'react';
 
 const CartPage = () => {
   const [open, setOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
   const { products, total, quantity } = useSelector((state) => state.cart);
   const currency = 'USD';
   const dispatch = useDispatch();
   const serviceCost = 4.0;
 
-  // const createOrder = async (data) => {
-  //   try {
-  //     const res = await axios.post('http://localhost:3000/api/orders', data);
-  //     if (res.status === 201) {
-  //       dispatch(reset());
-  //       router.push(`/orders/${res.data._id}`);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const createOrder = async (data) => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/orders', data);
+      if (res.status === 201) {
+        dispatch(reset());
+        router.push(`/orders/${res.data._id}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleRemoveProduct = (productId) => {
     dispatch(removeProduct({ id: productId }));
@@ -64,7 +66,7 @@ const CartPage = () => {
         <div className='bg-custom-blueblack overflow-x-hidden h-screen pr-2 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6 overflow-y-auto'>
           <div className='flex justify-between'>
             <span className=''>Subtotal({quantity})</span>
-            <span className='ml-2'> GH₵{total.toFixed(2)}</span>
+            <span className='ml-2'>{'GH₵' + total.toFixed(2)}</span>
           </div>
           <div className='flex justify-between'>
             <span className=''>Service Cost</span>
@@ -74,7 +76,7 @@ const CartPage = () => {
           </div>
           <div className='flex justify-between'>
             <span className=''>Delivery Cost</span>
-            <span className='text-green-500'>FREE! 🎉</span>
+            <span className='text-green-500'>GH₵{(0.0).toFixed(2)}</span>
           </div>
           <hr className='border-custom-orange' />
           <div className='flex justify-between text-custom-orange'>
@@ -106,12 +108,20 @@ const CartPage = () => {
           )}
           {products?.length === 0 && (
             <>
-              <div onClick={() => setOpen(true)}>
+              <div onClick={() => setIsModalOpen(true)}>
                 <Button label='PAY ON DElIVERY' />
               </div>
             </>
           )}
         </div>
+        {isModalOpen && (
+          <OrderForm
+            total={total}
+            createOrder={createOrder}
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+          />
+        )}
       </div>
       <Footer />
     </div>
