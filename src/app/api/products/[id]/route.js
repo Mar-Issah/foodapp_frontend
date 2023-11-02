a0d1e3f0791999352691185eb732ae1873a34bfc
Mar-Issah <masy370@gmail.com>
2023-11-02 16:37:57 -0500

    update: chnage the format of db_uri

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,17 +1,17 @@
-import Order from '@/models/Orders';
+import Product from '@/models/Product';
 import { NextResponse } from 'next/server';
 import { connectMongodb } from '@/lib/mongodb';
 
-connectMongodb();
+// connectMongodb();
 
-// GET SINGLE ORDER
+// GET SINGLE PRODUCT
 export const GET = async (req, { params }) => {
   const { method } = req;
-
+  await connectMongodb();
   if (method === 'GET') {
     try {
-      const orders = await Order.findById(params.id);
-      return new NextResponse(JSON.stringify(orders), { status: 200 });
+      const products = await Product.findById(params.id);
+      return new NextResponse(JSON.stringify(products), { status: 200 });
     } catch (err) {
       console.log(err);
       return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }), { status: 500 });
@@ -19,14 +19,15 @@ export const GET = async (req, { params }) => {
   }
 };
 
-// DELETE SINGLE ORDER
+// DELETE SINGLE PRODUCT
 export const DELETE = async (req, { params }) => {
   const { method } = req;
   // const { id } = params;
 
+  await connectMongodb();
   if (method === 'DELETE')
     try {
-      await Order.findByIdAndDelete(params.id);
+      await Product.findByIdAndDelete(params.id);
       return new NextResponse(JSON.stringify('Product has been deleted!'), {
         status: 200,
       });
