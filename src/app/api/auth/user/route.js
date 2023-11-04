5a3dc3ac1f7c4af544a9ec21686c8f33ef6cd1d3
Mar-Issah <masy370@gmail.com>
2023-11-04 00:47:50 -0500

    install bycrpt

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -1,10 +1,36 @@
 import { connectMongodb } from '@/lib/mongodb';
 import User from '@/models/user';
 import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
 
 export async function POST(request) {
   await connectMongodb();
   const body = await request.json();
-  const user = await User.create(body);
-  return NextResponse.json({ message: 'user created successfully' }, { status: 201 });
+
+  if (request.method === 'GET') {
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'already exists' });
+      }
+
+      if (body.email && body.password) {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(body.password, 10);
+        // Create a new user
+        const newUser = new User({
+          email,
+          password: hashedPassword, // Store the hashed password
+        });
+        await newUser.save();
+      } else {
+        const newUser = await User.create(body);
+      }
+      res.status(201).json({ message: 'User registered successfully' });
+      return NextResponse.json({ message: 'user created successfully' }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }), { status: 500 });
+    }
+  }
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { APP_URL } from '@/lib/url';
+import { useRouter } from 'next/navigation';
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +12,7 @@ const Signup = () => {
     password: '',
   });
   const [retryPassword, setRetry] = useState('');
+  const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -117,8 +119,6 @@ const Signup = () => {
             <div className='flex ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
               <input
                 type='password'
-                name='password2'
-                id='password2'
                 required
                 className={`block flex-1 border-0  py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 ${
                   retryPassword !== formData.password && 'bg-red-300'
