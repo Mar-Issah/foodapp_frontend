a8a2e97afe9ba9bd3a3d6940abc9688a93a3145f
Mar-Issah <masy370@gmail.com>
2023-11-04 15:17:16 -0500

    feat: add loading spinner to login page

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Spinner = () => {
+  return <div class='w-4 h-4 border-t-4 border-white border-solid rounded-full animate-spin'></div>;
+};
+
+export default Spinner;
