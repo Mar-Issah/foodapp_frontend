30eee7b3014a1c93e7f0645a393e7ba726ec1bfe
Mar-Issah <masy370@gmail.com>
2023-11-07 01:48:55 -0600

    refactor: add acitive link style on navbar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,10 +4,15 @@ import Link from 'next/link';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-const CartIcon = () => {
+const CartIcon = ({ currentRoute }) => {
   const quantity = useSelector((state) => state.cart.quantity);
   return (
-    <Link href='/cart' className='flex items-center gap-1 hover:border-b-2 border-custom-orange'>
+    <Link
+      href='/cart'
+      className={`${
+        currentRoute === '/cart' && 'border-b-2 border-custom-orange'
+      } flex items-center gap-1 hover:text-custom-orange`}
+    >
       <div className='relative w-8 h-8 md:w-5 md:h-5'>
         <FontAwesomeIcon icon={faCartShopping} style={{ color: '#bfc7d4' }} />
       </div>
