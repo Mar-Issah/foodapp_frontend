784b0457dda77dcf35c849c62dd995565ef648a0
Mar-Issah <masy370@gmail.com>
2023-11-13 12:20:03 -0600

    refactor: add comments to notice component

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+//a component that returns a scrolling text
 const Notice = () => {
   return (
     <div className='bg-black'>
-      <div className='text-custom-orange animate-notice'>🥳🥂🎉Get 30% discount off all dishes if your are a first-timer. Enjoy!🥳🥂🎉</div>
+      <div className='text-custom-orange animate-notice'>
+        🥳🥂🎉Get 30% discount off all dishes if your are a first-timer. Enjoy!🥳🥂🎉
+      </div>
     </div>
   );
 };
