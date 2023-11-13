e89d4b0dc91adafcb4051fdb0508c276f7d2812b
Mar-Issah <masy370@gmail.com>
2023-11-13 12:13:47 -0600

    refactor: add comments to button components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import Welcome from '@/components/Welcome';
 
+// project home page
 export default function Home() {
   return (
     <main className='w-screen'>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Button = ({ label, ...props }) => {
+//button components take a label prop
+const Button = ({ label }) => {
   return (
     <button className='bg-transparent text-gray-100 p-3 border-2 border-custom-orange rounded-full shadow-lg hover:bg-custom-orange'>
       {label}
